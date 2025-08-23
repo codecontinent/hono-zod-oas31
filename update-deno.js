386a5c5ae0921d6ef6ebe6c941bef158d337dd70
Mr Meaow <0306mahabub@gmail.com>
2025-08-23 11:34:44 +0600

    fix: v1.1.1 stable release

--- update-deno.js
+++ update-deno.js
@@ -1,5 +1,5 @@
-// eslint-disable-next-line n/no-extraneous-import
 import chalk from 'chalk'
+import { spawnSync } from 'child_process'
 import fs from 'fs'
 
 const version = process.argv[2]
@@ -8,5 +8,6 @@ const filePath = 'deno.json'
 const denoJson = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
 denoJson.version = version
 fs.writeFileSync(filePath, JSON.stringify(denoJson, null, 2) + '\n')
+spawnSync('pnpm', ['prettier', '--write', 'deno.json'], { stdio: 'inherit' })
 
 console.log(chalk.green(`✔ deno.json updated to version ${version}`))
